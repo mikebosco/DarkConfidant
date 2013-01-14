9267319e112586bd706a152a536715f2e2d6121a
Michael Bosco <mbosco@university-bound.com>
2013-01-13 20:49:06 -0500

    Rails barf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DarkConfidant::Application.config.secret_token = '849839d3bd1da4bae34ba17344aabe420f54a6aa4a1fa6f0e0dce15b6c3141e47c47c9063b4553c87735897d9151b76e97beffce49912ce536387f4634cd7b57'
